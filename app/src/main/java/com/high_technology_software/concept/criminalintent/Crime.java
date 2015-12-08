472a438edff0b5811b8edbe98449e6a9d6baf35d
foxtrot-developer <albertmotosgonzalez@gmail.com>
2015-12-08 18:38:08 +0100

    Portrait layout added

--- app/src/main/java/com/high_technology_software/concept/criminalintent/Crime.java
+++ app/src/main/java/com/high_technology_software/concept/criminalintent/Crime.java
@@ -1,15 +1,18 @@
 package com.high_technology_software.concept.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
-        // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -23,4 +26,21 @@ public class Crime {
     public void setTitle(String title) {
         mTitle = title;
     }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 }
