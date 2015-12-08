32480e703c006c416c5318067770b1329f137c68
foxtrot-developer <albertmotosgonzalez@gmail.com>
2015-12-08 16:36:31 +0100

    Initial commit

---
+++ app/src/main/java/com/high_technology_software/concept/criminalintent/CrimeFragment.java
@@ -0,0 +1,48 @@
+package com.high_technology_software.concept.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
+    }
+}
