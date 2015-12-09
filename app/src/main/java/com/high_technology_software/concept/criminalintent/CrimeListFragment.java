7775cd9aea8137f85d2598b0798b67c513016142
foxtrot-developer <albertmotosgonzalez@gmail.com>
2015-12-09 12:23:59 +0100

    Using fragment arguments

--- app/src/main/java/com/high_technology_software/concept/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/high_technology_software/concept/criminalintent/CrimeListFragment.java
@@ -1,18 +1,19 @@
 package com.high_technology_software.concept.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment {
-
-    private static final String TAG = "CrimeListFragment";
 
     private ArrayList<Crime> mCrimes;
 
@@ -22,17 +23,17 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
 
-        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
-                android.R.layout.simple_list_item_1,
-                mCrimes);
-
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Crime c = (Crime)(getListAdapter()).getItem(position);
-        Log.d(TAG, c.getTitle() + " was clicked");
+        Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
+
+        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+        startActivity(i);
     }
 
     private class CrimeAdapter extends ArrayAdapter<Crime> {
@@ -43,7 +44,27 @@ public class CrimeListFragment extends ListFragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return super.getView(position, convertView, parent);
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().
+                        inflate(R.layout.list_item_crime, null);
+            }
+
+            Crime c = getItem(position);
+
+            TextView titleTextView = (TextView) convertView. findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
     }
 }
